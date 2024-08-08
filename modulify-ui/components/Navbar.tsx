65d4ce2733ec1b50fe33b63fd62ab9062d291f82
hugocnl11 <hugocnl11@gmail.com>
2024-08-08 21:11:23 +0200

    v0.0.2
    - Addition component navegation bar.

---
+++ modulify-ui/components/Navbar.tsx
@@ -0,0 +1,31 @@
+// components/Navbar.tsx
+
+import React from 'react';
+import Link from 'next/link';
+
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-gray-970 p-4 shadow-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold bg-gradient-text bg-clip-text text-transparent">
+          <Link href="/">Modulify UI</Link> 
+        </div>
+        
+        <div className="space-x-4">
+          <Link href="/about" className="text-white hover:text-gray-300 transition-colors duration-200">
+            Components
+          </Link>
+          <Link href="/services" className="text-white hover:text-gray-300 transition-colors duration-200">
+            Services
+          </Link>
+          <Link href="/contact" className="text-white hover:text-gray-300 transition-colors duration-200">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- modulify-ui/tailwind.config.ts
+++ modulify-ui/tailwind.config.ts
@@ -12,9 +12,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-text": "linear-gradient(90deg, #ff00cc, #333399, #00ccff)",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
